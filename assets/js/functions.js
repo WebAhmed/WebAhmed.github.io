834fcd730540e17555fdb920576f319a7f1985f8
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-03-23 21:01:17 -0700

    add paralax   - added parallax section   - removed unused images   - added style to parallax section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
 
-  /* Full-page initialization */
-  $('#fullpage').fullpage({
-    //Accessibility
-    keyboardScrolling: true,
-    animateAnchor: true,
-    recordHistory: true,
-    css3: true,
-    // menu: '#menu',
-    anchors: ['homePage', 'aboutPage', 'skillsPage', 'contactPage'],
-    verticalCentered: true, /* center the screen*/
-    navigation: true,
-    navigationPosition: 'right',
-    easing: 'easeInOutCubic',
-    scrollOverflow: true,
-    scrollingSpeed: 700
-  });
+  // /* Full-page initialization */
+  // $('#fullpage').fullpage({
+  //   //Accessibility
+  //   keyboardScrolling: true,
+  //   animateAnchor: true,
+  //   recordHistory: true,
+  //   css3: true,
+  //   // menu: '#menu',
+  //   anchors: ['homePage', 'aboutPage', 'skillsPage', 'contactPage'],
+  //   verticalCentered: true, /* center the screen*/
+  //   navigation: true,
+  //   navigationPosition: 'right',
+  //   easing: 'easeInOutCubic',
+  //   scrollOverflow: true,
+  //   scrollingSpeed: 700
+  // });
+    $('#skills-page').parallax();
 });
