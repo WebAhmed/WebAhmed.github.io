feea1f6806c95675698c49e333310c691d187a6a
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-05-04 22:36:17 -0700

    prep for bootstrap

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-
-  /* Full-page initialization */
-  $('#main-container').fullContent({ 
-    stages: 'div', 
-    mapPosition: [{v: 1, h: 1}, {v: 1, h: 2}, {v: 2, h: 1}, {v: 2, h: 2}], 
-    stageStart: 1, 
-    speedTransition: 800, 
-    idComplement: 'page_', 
-    ease: 'easeOutQuad' 
-  });
-
   $('#skills-page').parallax();
 });
