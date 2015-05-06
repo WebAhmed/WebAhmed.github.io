f034eb03c60f82e7e38d800fd88e5f96b6ad16cf
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-05-05 21:21:35 -0700

    finish styling index's header and nav

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+  var homeParallaxInfo = $('#homeParallaxInfo')
   $('#skills-page').parallax();
+
+  $(window).scroll(function(){
+    var wScroll = $(this).scrollTop()
+    homeParallaxInfo.css({
+      'transform' : 'translate(0px,'+ wScroll /2 +'%)'
+    })
+  })
 });
