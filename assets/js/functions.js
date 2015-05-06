028c19d80621dcec6da7c74b2ba59086fa47242a
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-05-06 12:07:20 -0700

    add animate.css parallax and finish styling index

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
   var homeParallaxInfo = $('#homeParallaxInfo')
-  $('#skills-page').parallax();
+  var aboutMe = $('#about-me')
+  var figure = $('.figure')
+  var intro = $('.intro')
+  
+
+//parallax
+
+  intro.addClass('animated fadeInUp')
 
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop()
+    var aboutMeOffset = aboutMe.offset().top
+
+
     homeParallaxInfo.css({
       'transform' : 'translate(0px,'+ wScroll /2 +'%)'
     })
+
+    if (aboutMeOffset > wScroll ) {
+      figure.addClass('animated fadeInUp')
+    } 
+
   })
+
+  $('#skills-page').parallax();
+
 });
